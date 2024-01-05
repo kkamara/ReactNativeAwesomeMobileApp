7cde8d095aab6e1a2b9874fd5c236f2503144bf2
kkamara <kamaracomputers@gmail.com>
2024-01-05 10:51:39 +0000

    Create app home screen

---
+++ AppPro.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import {
+    View,
+    Text,
+    StyleSheet,
+    useColorScheme,
+} from "react-native"
+
+export default function AppPro(): JSX.Element {
+    const isDarkMode = useColorScheme() === "dark"
+
+    return (
+        <View style={styles.container}>
+            <Text 
+                style={isDarkMode ? styles.darkText : styles.darkText}
+            >
+                Hello
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // alignItems: "flex-end",
+        alignItems: "center",
+        justifyContent: "center",
+        // paddingHorizontal: 20,
+    },
+    whiteText: {
+        color: "#fff",
+    },
+    darkText: {
+        color: "#000",
+    },
+})
